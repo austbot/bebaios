100a29139e7e1b9500d0d1dc2bff41461c969dcb
austbot <me@austbot.com>
2018-11-12 20:34:29 -0800

    server is there

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,5 @@ RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix nocgo -o /app .
 
 FROM scratch
 COPY --from=builder /app ./
+EXPOSE 8080
 ENTRYPOINT ["./app"]
--- backend/server.go
+++ backend/server.go
@@ -2,20 +2,55 @@ package backend
 
 import (
 	"github.com/gin-gonic/gin"
+	"k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	k8sRest "k8s.io/client-go/rest"
+	"net/http"
 )
 
-func StartApiServer(router gin.IRouter, config *k8sRest.Config) {
-	//clientset, err := kubernetes.NewForConfig(config)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+type DumpResp struct {
+	Namespaces DumpNsMap `json:"namespaces"`
+}
+type DumpNsMap = map[string]DumpNs
+type DumpNs struct {
+	Namespace v1.Namespace    `json:"namespace"`
+	Roles     rbacv1.RoleList `json:"roles"`
+}
 
-	//sa, error := clientset.RbacV1().ClusterRoles().List(v1.ListOptions{})
+func StartApiServer(router gin.IRouter, config *k8sRest.Config) {
 	r := router.Group("/api")
+	r.GET("/dump", func(c *gin.Context) {
+		clienSet, configError := kubernetes.NewForConfig(config)
+		handleIfError(c, configError)
+		ns, nsError := clienSet.CoreV1().Namespaces().List(metav1.ListOptions{})
+		handleIfError(c, nsError)
+		var roles = map[string]DumpNs{}
+		for _, namespace := range ns.Items {
+			roleList, error := clienSet.RbacV1().Roles(namespace.Name).List(metav1.ListOptions{})
+			handleIfError(c, error)
+			roles[namespace.Name] = DumpNs{
+				Namespace: namespace,
+				Roles:     *roleList,
+			}
+		}
+		c.JSON(200, DumpResp{
+			Namespaces: roles,
+		})
+	})
+
 	r.GET("/healthz", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+}
+
+func handleIfError(c *gin.Context, err error) {
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error": c.Error(err)})
+		c.Next()
+		return
+	}
 }
